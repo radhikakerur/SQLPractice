cbfc4195415e95fbe754a864343d60b18c2e1463
Radhika Kerur <radhikakerur@gmail.com>
2024-02-03 16:54:08 -0700

    Update SS: User with Most Approved Flags.sql

--- SS: User with Most Approved Flags.sql
+++ SS: User with Most Approved Flags.sql
@@ -7,6 +7,7 @@ n the user's full name, include a space between the first and the last name.
 select username 
 from (
         select concat(user_firstname,' ', user_lastname) as username
+        -- Postgres allows using aggregation function within window functions
             , dense_rank()over(order by count(distinct u.video_id) desc)dr
         from user_flags u
         join (select * from flag_review where reviewed_outcome = 'APPROVED') f
