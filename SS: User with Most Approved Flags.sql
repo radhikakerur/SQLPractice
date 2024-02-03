c1edc0195777e3ef09bb81b1a42f3f3542179149
Radhika Kerur <radhikakerur@gmail.com>
2024-02-03 16:52:16 -0700

    Create SS: User with Most Approved Flags.sql

---
+++ SS: User with Most Approved Flags.sql
@@ -0,0 +1,16 @@
+/*
+Which user flagged the most distinct videos that ended up approved by YouTube? 
+Output, in one column, their full name or names in case of a tie. I
+n the user's full name, include a space between the first and the last name.
+*/
+
+select username 
+from (
+        select concat(user_firstname,' ', user_lastname) as username
+            , dense_rank()over(order by count(distinct u.video_id) desc)dr
+        from user_flags u
+        join (select * from flag_review where reviewed_outcome = 'APPROVED') f
+        on u.flag_id = f.flag_id
+        group by concat(user_firstname,' ', user_lastname)
+    )tmp
+where dr =1
